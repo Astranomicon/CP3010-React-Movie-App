d8f07168881e4b105e0ca98e2803e6ebb52a206e
Paul <william.paul13@ed.cna.nl.ca>
2023-02-10 12:46:50 -0500

    Updated Navbar

--- movie-app/src/components/Navbar.js
+++ movie-app/src/components/Navbar.js
@@ -1,10 +1,25 @@
-import { Link } from 'react-router-dom'
+import { NavLink } from "react-router-dom";
 
 export const Navbar = () => {
-  return (
-    <nav>
-        <Link to="/">Home</Link>
-        <Link to="/reviews">Add Review</Link>
-    </nav>
-  )
-}
+  
+	const navLinkStyles = ({ isActive }) => {
+		return {
+			fontWeight: isActive ? "bold" : "normal",
+			textDecoration: isActive ? "none" : "underline",
+			border: isActive ? "1px solid black" : "none",
+			padding: isActive ? "4px" : "0px",
+			borderRadius: isActive ? "6px" : "0px",
+		};
+	};
+
+	return (
+		<nav>
+			<NavLink style={navLinkStyles} to='/'>
+				Home
+			</NavLink>
+			<NavLink style={navLinkStyles} to='/reviews'>
+				Add Review
+			</NavLink>
+		</nav>
+	);
+};
