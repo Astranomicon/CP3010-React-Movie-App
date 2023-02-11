82791ec5b9f4be8776046cae6ceff44ddef7f034
Paul <william.paul13@ed.cna.nl.ca>
2023-02-10 19:46:38 -0500

    Added form components

---
+++ movie-app/src/components/Button.js
@@ -0,0 +1,7 @@
+export const Button = ({ text }) => {
+	return (
+		<button className='btn'>
+			{text}
+		</button>
+	);
+};
--- movie-app/src/views/Reviews.js
+++ movie-app/src/views/Reviews.js
@@ -1,11 +1,44 @@
-import React from 'react'
+import React from "react";
+import { Button } from "../components/Button";
 
 export const Reviews = () => {
-  return (
-    <div>
-      <h3>
-        Review Form Page
-      </h3>
-    </div>
-  )
-}
+	return (
+		<div>
+			<form>
+				<div className='form-container'>
+					<input
+						type='text'
+						id='movieName'
+						name='movieName'
+						placeholder='Movie Name'
+					/>
+					<input
+						type='text'
+						id='movieReleaseDate'
+						name='movieReleaseDate'
+						placeholder='Release Date'
+					/>
+					<input
+						type='text'
+						id='movieActors'
+						name='movieActors'
+						placeholder='Actors'
+					/>
+					<select name='moviePoster'>
+						<option value='1'>Default Image</option>
+						<option value='2'>Upload Image</option>
+					</select>
+
+					<select name='movieRating'>
+						<option value='1'>1 Star</option>
+						<option value='2'>2 Stars</option>
+						<option value='3'>3 Stars</option>
+						<option value='4'>4 Stars</option>
+						<option value='5'>5 Stars</option>
+					</select>
+				</div>
+			</form>
+			<Button text='Add Movie Review' />
+		</div>
+	);
+};
